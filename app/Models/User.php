8447f371164d8e671e5945797e9c3786e7066bfa
Almario Miano <almario.dev@gmail.com>
2024-03-08 17:00:44 -0600

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,6 +35,9 @@ class User extends Authenticatable
         'remember_token',
         'deleted_at',
         'locked_at',
+        'created_at',
+        'updated_at',
+        'email_verified_at',
     ];
 
     /**
