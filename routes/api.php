e2d68737b2c13d091adb018bd9c192f70615bf3c
Almario Miano <almario.dev@gmail.com>
2024-03-08 17:01:02 -0600

    applied middlewares

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(AuthController::class)->prefix('auth')->group(function() {
-    Route::post('', 'test');
-    Route::post('create', 'createUser');
+
+Route::controller(AuthController::class)->group(function() {
+    Route::post('login', 'login')->name('panelLogin');
+    Route::middleware('auth:sanctum')->get('refresh-auth', 'refreshAuth');
+    Route::middleware('auth:sanctum')->get('logout', 'logout');
 });
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->group(function () {
+//
+});
