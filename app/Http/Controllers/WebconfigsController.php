efc5be7a2155996e3636fce92a1c3110be20c801
Almario Miano <almario.dev@gmail.com>
2024-03-09 19:59:35 -0600

    Create WebconfigsController.php

---
+++ app/Http/Controllers/WebconfigsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Webconfigs;
+use Illuminate\Http\Request;
+
+class WebconfigsController extends Controller
+{
+   public function index() {
+    
+   }
+}
