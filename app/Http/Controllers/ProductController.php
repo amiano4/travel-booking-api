698cb470105c3f5256e93f170bd0d44c8b4aff5b
Almario Miano <almario.dev@gmail.com>
2024-03-10 15:12:12 -0500

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,29 +3,64 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\ProductPackage;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
+    public function productsByVendor() {
+        $user = User::find(Auth::user()->id);
+        return response()->json([
+            'products' => $user->products,
+            'packages' => $user->productpackages,
+        ]);
+    }
+
     public function add(Request $request) {
         $request->validate([
             'item' => 'required',
             'rate' => 'numeric|min:0',
-            'package' => 'nullable|exists:product_packages,id',
-            'description' => 'string|nullable'
+            'package' => 'nullable',
+            'description' => 'string|nullable',
+            'package_name' => 'string|nullable',
+            'package_details' => 'string|nullable',
         ]);
 
-        if($product = Product::create([
+        $pkgId = $request->package;
+
+        if($request->package == 'new') {
+            $pkg = ProductPackage::create([
+                'client_id' => Auth::user()->id,
+                'name' => $request->package_name,
+                'details' => $request->package_details,
+            ]);
+
+            $pkgId = $pkg->id;
+        }
+
+        if(Product::create([
             'client_id' => Auth::user()->id,
-            'package_id' => $request->package,
+            'package_id' => $pkgId,
             'item' => $request->item,
             'rate' => $request->rate,
             'description' => $request->description
         ])) {
-            return response()->json('Successfully added.');
+            return $this->productsByVendor();
         }
 
         return response()->json('Error.', 500);
+    }
+
+    public function flag(Product $product, $status) {
+        $user = Auth::user();
+
+        foreach($user->products as $p) {
+            $p->update(['flag' => null]);
+        }
+
+        $product->update(['flag' => $status]);
+        return $this->productsByVendor();
     }
 }
