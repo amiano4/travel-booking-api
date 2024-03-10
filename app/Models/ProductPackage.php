d51a851122565a6bc878c92f525f7b58f40873b3
Almario Miano <almario.dev@gmail.com>
2024-03-09 22:16:02 -0600

    Create ProductPackage.php

---
+++ app/Models/ProductPackage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductPackage extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'client_id',
+        'details',
+    ];
+
+    protected $casts = [
+        'details' => 'array',
+    ];
+
+    public function vendor() {
+        return $this->belongsTo(User::class, 'client_id');
+    }
+
+    public function products() {
+        return $this->hasMany(Product::class, 'package_id');
+    }
+}
